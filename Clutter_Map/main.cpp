a4eac60515aa77edb2f8273ee9b06ecc5d48bab5
Darkus526 <39304963+Darkus526@users.noreply.github.com>
2020-07-20 12:45:54 +0700

    Code update

--- Clutter_Map/main.cpp
+++ Clutter_Map/main.cpp
@@ -14,18 +14,18 @@ int main(int argc, char *argv[])
 
     int i, a=0, N=10;
 
+    for (i=0; i<N; i++)
+{
+    vec.push_back(i+1);
+}
+
     int *KT=vec.data();
 
-    for (i=0; i<=N; i++)
+    for (i=0; i<N; i++)
 {
-    cout << KT[i];
-    if (i>0)
-    {
-    KT[i-1]=KT[i-1]*10;
-    }
+    KT[i]=KT[i]*2;
 }
 
     qDebug()<<vec;
-
     return app.exec();
 }
